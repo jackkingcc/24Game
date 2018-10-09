1530833745708b7aa09e2971d9ec00f598bfc304
e604847 <jjin4@statestreet.com>
2018-10-09 17:13:21 +0800

    First version

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**
- * Created by e604847 on 10/9/2018.
- */
 public class Calculator {
 
     /*
@@ -14,21 +11,141 @@ public class Calculator {
     @m max value of input
      */
     public List<String> calculate(int n, int m){
-        List<Integer> inputs = new ArrayList<Integer>(n);
-        for(int i=0;i<n;i++){
-            inputs.add(createRandom(m));
+        List<String> res=null;
+        boolean isValid=false;
+        while(!isValid) {
+            List<Integer> inputs = createRandom(n,m);
+            inputs.sort((i1, i2) -> i1 - i2);
+            res = calculate(inputs);
+            if(res.size()!=0) isValid=true;
         }
-        inputs.stream();
-        List<String> res = calculate(inputs);
-        return res;
-    }
-    public List<String> calculate(List<Integer> inputs){
-        List<String> res = new ArrayList();
         return res;
     }
 
-    private int createRandom(int m){
+    public List<String> calculate(List<Integer> inputs){
+        List<String> resultArr = new ArrayList();
+        List<List<Integer>> inputsList = buildInputs(inputs);
+        for(int i=0;i<inputsList.size();i++) {
+            inputs = inputsList.get(i);
+            int i1 =inputs.get(0);
+            int i2 =inputs.get(1);
+            int i3 =inputs.get(2);
+            int i4 =inputs.get(3);
+            double x = inputs.get(0);
+            double y = inputs.get(1);
+            double z = inputs.get(2);
+            double w = inputs.get(3);
+            if (x + y + z + w == 24) {
+                String aResult = i1 + "+" + i2 + "+" + i3 + "+" + i4;
+                resultArr.add(aResult);
+            } else if (x + y + z - w == 24) {
+                String aResult = i1 + "+" + i2 + "+" + i3 + "-" + i4;
+                resultArr.add(aResult);
+            } else if ((x + y) * (z + w) == 24) {
+                String aResult = "(" + i1 + "+" + i2 + ")*(" + i3 + "+" + i4 + ")";
+                resultArr.add(aResult);
+            } else if ((x - y) * (z + w) == 24) {
+                String aResult = "(" + i1 + "-" + i2 + ")*(" + i3 + "+" + i4 + ")";
+                resultArr.add(aResult);
+            } else if ((x - y) * (z - w) == 24) {
+                String aResult = "(" + i1 + "-" + i2 + ")*(" + i3 + "-" + i4 + ")";
+                resultArr.add(aResult);
+            } else if ((x + y + z) * w == 24) {
+                String aResult = "(" + i1 + "+" + i2 + "+" + i3 + ")*" + i4;
+                resultArr.add(aResult);
+            } else if ((x - y - z) * w == 24) {
+                String aResult = "(" + i1 + "-" + i2 + "-" + i3 + ")*" + i4;
+                resultArr.add(aResult);
+            } else if ((x + y - z) * w == 24) {
+                String aResult = "(" + i1 + "+" + i2 + "-" + i3 + ")*" + i4;
+                resultArr.add(aResult);
+            } else if ((x * y * z) / w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + "*" + i3 + ")/" + i4;
+                resultArr.add(aResult);
+            } else if (x * y * (z + w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")*(" + i3 + "+" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y * (z - w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")*(" + i3 + "-" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y * z - w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")*(" + i3 + ")-" + i4;
+                resultArr.add(aResult);
+            } else if (x * y * z + w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")*(" + i3 + ")+" + i4;
+                resultArr.add(aResult);
+            } else if (x * y * z * w == 24) {
+                String aResult = i1 + "*" + i2 + "*" + i3 + "*" + i4;
+                resultArr.add(aResult);
+            } else if ((x + y) + (z / w) == 24) {
+                String aResult = "(" + i1 + "+" + i2 + ")+(" + i3 + "/" + i4 + ")";
+                resultArr.add(aResult);
+            } else if ((x + y) * (z / w) == 24) {
+                String aResult = "(" + i1 + "+" + i2 + ")*(" + i3 + "/" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y + z + w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")+" + i3 + "+" + i4;
+                resultArr.add(aResult);
+            } else if (x * y + z - w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")+" + i3 + "-" + i4;
+                resultArr.add(aResult);
+            } else if (x * y - (z / w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")-(" + i3 + "/" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y + (z / w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")-(" + i3 + "/" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y - z - w == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")-" + i3 + "-" + i4;
+                resultArr.add(aResult);
+            } else if (x * y + (z * w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")+(" + i3 + "*" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y - (z * w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")-(" + i3 + "*" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y / (z * w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")/(" + i3 + "*" + i4 + ")";
+                resultArr.add(aResult);
+            } else if ((z - w) != 0 && x * y / (z - w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")/(" + i3 + "-" + i4 + ")";
+                resultArr.add(aResult);
+            } else if (x * y / (z + w) == 24) {
+                String aResult = "(" + i1 + "*" + i2 + ")/(" + i3 + "+" + i4 + ")";
+                resultArr.add(aResult);
+            }
+        }
+        return resultArr;
+    }
+
+    private List<List<Integer>> buildInputs(List<Integer> inputs) {
+        List<List<Integer>> inputsList = new ArrayList<>();
+        for(int i=0;i<inputs.size();i++){
+            for(int j=0;j<inputs.size();j++){
+                for(int k=0;k<inputs.size();k++){
+                    for(int p=0;p<inputs.size();p++){
+                        if(i!=j && i!=k && i!=p && j!=k && j!=p && k!=p){
+                            List<Integer> newList = new ArrayList<>();
+                            newList.add(inputs.get(i));
+                            newList.add(inputs.get(j));
+                            newList.add(inputs.get(k));
+                            newList.add(inputs.get(p));
+                            if(!inputsList.contains(newList))
+                                inputsList.add(newList);
+                        }
+                    }
+                }
+            }
+        }
+        return inputsList;
+    }
+
+    public List<Integer> createRandom(int n, int m){
+        List<Integer> res=new ArrayList<>(n);
         Random random = new Random();
-        return random.nextInt(m+1);
+        for(int i=0;i<n;i++) {
+             res.add(random.nextInt(m) + 1);
+        }
+        return res;
     }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,14 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.util.Duration;
+
+import java.util.List;
 
 public class Controller {
     @FXML
@@ -29,8 +34,26 @@ public class Controller {
 
     @FXML
     private void printOut(){
-        outputText1.setText("HaHa");
-        outputText2.setText("HeHe");
+        int n=4,m=10;
+        Calculator calculator = new Calculator();
+        List<Integer> inputs=calculator.createRandom(n,m);
+        outputText1.setText(inputs.toString());
+        List<String> res= calculator.calculate(inputs);
+        while(res.size()==0){
+            res=calculator.calculate(inputs);
+        }
+        final  String text1 ="There are "+res.size()+" ways";
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5)));
+        Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(10)));
+        StringBuilder display = new StringBuilder();
+        for(String str:res){
+            display.append(str+"\n");
+        }
+        timeline2.setOnFinished(event -> outputText2.setText(display.toString()));
+        timeline.setOnFinished(event ->{outputText2.setText(text1);timeline2.play();
+        });
+        timeline.play();
+
 
     }
 
